282e8b6275b72e4620f4eeb314e0bc3f78ffaff1
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-01-27 22:03:35 +0800

    test: get yaml file path

---
+++ gimp-palette/files.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"path"
+	"runtime"
+)
+
+var (
+	_, file, _, _ = runtime.Caller(0)
+	dataDir       = path.Join(path.Dir(file), "..", "data")
+)
+
+var (
+	MTRLogoYAMLFile      = path.Join(dataDir, "mtr-logo.yaml")
+	MTRSystemMapYAMLFile = path.Join(dataDir, "system-map.yaml")
+)
---
+++ gimp-palette/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	file := MTRSystemMapYAMLFile
+	fmt.Printf("%s\n", file)
+}
