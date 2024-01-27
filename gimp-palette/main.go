1269cc761cf91fd2fb46dc11792dbe5fa5d977a4
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-01-28 01:10:14 +0800

    feat: read MTR logo data and generate GIMP palette

--- gimp-palette/main.go
+++ gimp-palette/main.go
@@ -1,14 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gbarail/mtr-colors/types"
+)
 
 func main() {
+	palette := types.GIMPPalette{
+		Name: "Hong Kong MTR colors",
+	}
+
 	data, err := ReadMTRLogoYAMLFile()
 	if err != nil {
 		panic(err)
 	}
 
-	for k, v := range data {
-		fmt.Printf("%s: %s [%d %d %d]\n", k, v.Name, v.RGB[0], v.RGB[1], v.RGB[2])
+	category := types.Category{
+		Name:   "MTR logo",
+		Colors: []*types.Color{},
 	}
+
+	for k, v := range data {
+		category.Colors = append(category.Colors, &types.Color{
+			Name: k,
+			RGB:  v.RGB,
+		})
+	}
+
+	palette.Categories = append(palette.Categories, &category)
+
+	fmt.Printf("%v\n", palette)
 }
--- gimp-palette/types/gimp.go
+++ gimp-palette/types/gimp.go
@@ -1,8 +1,46 @@
 package types
 
+import (
+	"fmt"
+	"strings"
+)
+
 type Category struct {
 	Name   string
 	Colors []*Color
 }
 
-type GIMPPalette []*Category
+type GIMPPalette struct {
+	Name       string
+	Categories []*Category
+}
+
+const GIMPPaletteTop = "GIMP Palette"
+
+func (p GIMPPalette) String() string {
+	data := make([]string, 256)
+
+	top := fmt.Sprintf("%s\nName: %s\n\n", GIMPPaletteTop, p.Name)
+	data = append(data, top)
+
+	for _, category := range p.Categories {
+		if category.Name != "" {
+			data = append(data, fmt.Sprintf("# %s\n", category.Name))
+		} else {
+			data = append(data, "\n")
+		}
+
+		for _, color := range category.Colors {
+			RGB := color.RGB
+			data = append(data, fmt.Sprintf("%3d %3d %3d", RGB[0], RGB[1], RGB[2]))
+
+			if color.Name != "" {
+				data = append(data, fmt.Sprintf(" # %s\n", color.Name))
+			} else {
+				data = append(data, "\n")
+			}
+		}
+	}
+
+	return strings.Join(data, "")
+}
