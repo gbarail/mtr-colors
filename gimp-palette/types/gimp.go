dda0096607a8d4a2089b75cc0dde02e22acea2da
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-01-28 15:11:04 +0800

    chore: tweak output for GIMP palette

--- gimp-palette/types/gimp.go
+++ gimp-palette/types/gimp.go
@@ -15,21 +15,24 @@ type GIMPPalette struct {
 	Categories []*GIMPPaletteCategory
 }
 
-const GIMPPaletteTop = "GIMP Palette"
-
 func (p GIMPPalette) String() string {
 	data := make([]string, 256)
 
-	top := fmt.Sprintf("%s\nName: %s\n\n", GIMPPaletteTop, p.Name)
+	// Output top
+	top := fmt.Sprintf("GIMP Palette\nName: %s\n", p.Name)
 	data = append(data, top)
 
 	for _, category := range p.Categories {
+		data = append(data, "\n")
+
+		// Output category name if it exists
 		if category.Name != "" {
 			data = append(data, fmt.Sprintf("# %s\n", category.Name))
 		} else {
 			data = append(data, "\n")
 		}
 
+		// Output colors
 		for _, color := range category.Colors {
 			RGB := color.RGB
 			data = append(data, fmt.Sprintf("%3d %3d %3d", RGB[0], RGB[1], RGB[2]))
