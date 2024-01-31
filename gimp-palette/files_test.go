605c4d3e31b7f04786429326d6c61af824561124
mtrhk <76126473+mtrhk@users.noreply.github.com>
2024-01-31 10:13:12 +0800

    test: `TestReadMTRSystemMapYAMLFile`

--- gimp-palette/files_test.go
+++ gimp-palette/files_test.go
@@ -12,3 +12,11 @@ func TestReadMTRLogoYAMLFile(t *testing.T) {
 
 	assert.Equal(t, len(data), 3) // 3 colors in MTR logo
 }
+
+func TestReadMTRSystemMapYAMLFile(t *testing.T) {
+	data, err := ReadMTRSystemMapYAMLFile()
+	assert.NilError(t, err)
+
+	assert.Assert(t, len(data.LinesColors) > 0)
+	assert.Assert(t, len(data.MiscellaneousColors) > 0)
+}
